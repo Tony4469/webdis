f833d6ab5482ffb8f9481bb2e447ec1ac6afdc07
Tony4469 <souflam007@yahoo.fr>
2021-03-07 18:04:09 +0100

    add auth acl 5

--- webdis-wrapper.sh
+++ webdis-wrapper.sh
@@ -23,7 +23,8 @@ cat <<EOF > "$config_file"
       },
       {
          "http_basic_auth" : "user:password",
-         "enabled": ["*"]
+         "enabled": ["PUBLISH", "GET"],
+         "disabled": ["SET"]
       }
    ]
 }
